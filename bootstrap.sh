9513dcb0b10e6402dd141e0a4aedc66b90a2185a
wutangfinancial <35402234+wutangfinancial@users.noreply.github.com>
2018-07-06 23:22:36 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-curl https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | sudo bash
+curl https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | bash
